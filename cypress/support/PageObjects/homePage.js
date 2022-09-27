24ba687aa0bc249ce3e1a5f431a468402e8ea62c
Carlos Bernal <ramirez.bernal.jesus1@gmail.com>
2022-09-27 13:25:49 -0700

    imon

--- cypress/support/PageObjects/homePage.js
+++ cypress/support/PageObjects/homePage.js
@@ -7,5 +7,11 @@ class HomePage{
     getSearchBoxButton(){
         cy.get('#searchbox > .btn')
     }
+    getAddtoCartElementButton(){
+        cy.get('.product-container:has(.product-name[title="' + productDescription + '"]) .ajax_add_to_cart_button')
+    }
+    getProceedToCheckout(){
+        cy.get('.button-medium[title="Proceed to checkout"]')
+    }
 }
 export default HomePage;
